faee87cbda60f9472231b210879be100a7279f64
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2026-04-12 21:20:54 -0400

    unbreak fuzzer

--- scripts/build-for-fuzzer.ts
+++ scripts/build-for-fuzzer.ts
@@ -3,6 +3,7 @@ import plugin from "../src/esbuildPlugin";
 
 await build({
     platform: "node",
+    minify: true, // Must minify for the fs imports to be eliminated
     format: "cjs",
     entryPoints: { "backolon": "src/index.ts" },
     outExtension: { ".js": ".cjs" },
